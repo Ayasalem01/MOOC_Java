b27e6a277537a5ee206a8525b2adebf8e68f9d78
Aya Salem <112795469+Ayasalem01@users.noreply.github.com>
2022-11-22 19:06:58 +0100

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author lenovo
+ */
+public class MOOC {
+
+    public static void main(String[] args) {
+    division(3, 5);
+}
+
+    //the method should print the numbers from parameter toone
+    public static void division(int a, int b) {
+        
+        double dev= (double)a/b;
+        System.out.println(dev);
+
+    }
+}
